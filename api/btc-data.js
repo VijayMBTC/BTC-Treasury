4dec88060f8247b70c54ddaab7a4c290608f6542
Vijay <vijay@Vijays-MacBook-Pro.local>
2026-06-01 17:29:30 +0300

    fix proxy

--- api/btc-data.js
+++ api/btc-data.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
   try {
-    const response = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1500&interval=daily", { headers: { "Accept": "application/json", "x-cg-demo-api-key": "CG-b2qoMH8NfRuvJUJXohdMeFwb" } });
+    const response = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=365&interval=daily", { headers: { "Accept": "application/json", "x-cg-demo-api-key": "CG-b2qoMH8NfRuvJUJXohdMeFwb" } });
     const data = await response.json();
     res.setHeader("Cache-Control", "s-maxage=300");
     res.status(200).json(data);
